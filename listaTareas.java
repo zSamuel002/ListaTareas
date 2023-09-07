2d52604f988383cf94a558b4aec654a9a03a253a
zSamuel002 <143724951+zSamuel002@users.noreply.github.com>
2023-09-07 08:49:16 -0600

    ListaTareas

---
+++ listaTareas.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author Samuel
+ */
+public class listaTareas {
+    public static void main(String[] args) {
+      String tarea1 = "Convertir octal a decimal";
+      String tarea2 = "Hacer un algoritmo que resuelva la formula general";
+      String tarea3 = "Escribir un ensayo de 10 cuartillas sobre Apologia a Socrates";
+      String tarea4 = "Desarrollar una calculadora en java";
+      String tarea5 = "Calcular el area de un triangulo rectangulo";
+        
+        System.out.println("Lista de tareas: ");
+        System.out.println("La primera tarea es: " + tarea1);
+        System.out.println("La segunda tarea es: " + tarea2);
+        System.out.println("La tercera tarea es: " + tarea3);
+        System.out.println("La cuarta tarea es: " + tarea4);
+        System.out.println("La quinta tarea es: " + tarea5);
+    }
+    
+}
