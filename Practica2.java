53bf13f12d3920f957610943f27f089c18e9a5a7
zSamuel002 <143724951+zSamuel002@users.noreply.github.com>
2023-09-07 08:44:51 -0600

    Practica2

---
+++ Practica2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+    public static void main(String[] args) {
+        String nombre = "Samuel Emiliano Arriaga Garcia";
+        int base = 30;
+        int altura = 40;
+        
+        int area = base * altura;
+        
+        System.out.println("Mi nombre es es: " + nombre + ", y el area del rectangulo es: " + area );
+        
+        // Con decimales
+        System.out.println("Con numeros decimales");
+        double baseDecimales = 67.5;
+        double alturaDecimales = 47.4;
+        
+        double areaDecimales = baseDecimales * alturaDecimales;
+        System.out.println("Mi nombre es es: " + nombre + ", y el area del rectangulo es: " + areaDecimales );
+       
+    }
+    
+}
